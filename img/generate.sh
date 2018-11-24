c02a834d69e8748f3b0a8312200c9d271211bb57
Rune Jensen <nethandel@gmail.com>
2018-11-24 12:18:53 -0700

    converted doc images to jpg

--- img/generate.sh
+++ img/generate.sh
@@ -1,6 +1,9 @@
+#note:  requires imagemagick
+
 pdfdir='../tex'
 find $pdfdir -type f -name '*.pdf' -print0 |
-  while IFS= read -r -d '' file
+  while IFS= read -r -d '' file 
+  do 
     filename=`basename $file`
-    do convert -verbose -density 500 -resize 700 "${file}" "${filename%.*}.png"
+    convert -verbose -density 150 -quality 100 -alpha remove "${file}" "${filename%.*}.jpg"
   done
